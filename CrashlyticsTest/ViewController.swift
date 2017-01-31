74c6123ea9716a49b6888f82cb315f9b8109f051
tekkyuu <byabyabya.dx@gmail.com>
2017-01-31 15:31:01 +0900

    introduce crashlytics

--- CrashlyticsTest/ViewController.swift
+++ CrashlyticsTest/ViewController.swift
@@ -7,12 +7,20 @@
 //
 
 import UIKit
+import Crashlytics
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let button = UIButton(type: .roundedRect)
+        
+        button.frame = (frame: CGRect(x: self.view.frame.size.width - 60, y: 20, width: 50, height: 50))
+        button.setTitle("Crash", for: .normal)
+        button.addTarget(self, action: #selector(crashButtonTapped), for: .touchUpInside)
+        view.addSubview(button)
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func crashApplication(_ sender: Any) {
+        var arr = ["a", "b"]
+        print(arr[4])
+    }
+    @IBAction func crashButtonTapped(sender: AnyObject) {
+        Crashlytics.sharedInstance().crash()
+    }
 }
 
---
+++ build/Pods.build/Debug-iphoneos/Pods-CrashlyticsTest.build/DerivedSources/Pods_CrashlyticsTest_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_CrashlyticsTestVersionString[];
+ extern const double Pods_CrashlyticsTestVersionNumber;
+
+ const unsigned char Pods_CrashlyticsTestVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_CrashlyticsTest  PROJECT:Pods-1" "\n";
+ const double Pods_CrashlyticsTestVersionNumber __attribute__ ((used)) = (double)1.;
